58bbb70b297dc154a7d41a38ec4d0454c4d80fe4
tallal85 <58532026+tallal85@users.noreply.github.com>
2025-07-18 00:03:17 -0700

    Added basic SELECT queries for Customers, Products, and Orders

--- 03_dql/basic_queries.sql
+++ 03_dql/basic_queries.sql
@@ -1,1 +1,4 @@
--- Placeholder for basic_queries.sql
+-- Basic SELECT statements to view data
+SELECT * FROM SalesDW.Customers;
+SELECT * FROM SalesDW.Products;
+SELECT * FROM SalesDW.Orders;-- Placeholder for basic_queries.sql
